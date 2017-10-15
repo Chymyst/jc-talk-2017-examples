d97367617209b0cebbc71958583282475b546001
Sergei Winitzki <winitzki@gmail.com>
2017-10-14 21:56:11 -0700

    add project configs

---
+++ build.sbt
@@ -0,0 +1,8 @@
+scalaVersion := "2.12.3"
+
+name := "examples"
+
+libraryDependencies ++= Seq(
+  "io.chymyst" %% "chymyst-core" % "0.2.0",
+  "org.scalatest" %% "scalatest" % "3.0.4"
+)
---
+++ src/test/scala/io/chymyst/talk_examples/CounterSpec.scala
@@ -0,0 +1,9 @@
+package io.chymyst.talk_examples
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class CounterSpec extends FlatSpec with Matchers {
+
+  // Counter with blocking access
+  
+}
